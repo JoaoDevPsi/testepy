bf6edad54f3f3bbf00791794825630a863e4789b
JoaoDevPsi <167132977+JoaoDevPsi@users.noreply.github.com>
2024-12-05 11:47:57 -0300

    Add files via upload

---
+++ index13.py
@@ -0,0 +1,87 @@
+# usuario = input('insira seu nome de usuario: ')
+
+# senha = float(input('insira sua senha: '))
+
+# if usuario == 'admim' and senha == 1234:
+#     print ('login com sucesso')
+# else:
+#     print('erro, tente novamente')
+
+# ----------------------------------------------------------------
+
+# numero = float(input('insira seu número: '))
+
+# if numero %2 == 0:
+#     print('par')
+# else:
+#     print('impar')
+
+# -----------------------------------------------------------------
+
+# preco_original = float(input("Digite o preço original do produto: "))
+# quantidade = int(input("Digite a quantidade comprada: "))
+
+# total = preco_original * quantidade
+
+# if quantidade > 10:
+#     desconto = total * 0.1
+#     total_com_desconto = total - desconto
+#     print(f"O valor total com desconto de 10% é: R$ {total_com_desconto:.2f}")
+# else:
+#     print(f"O valor total é: R$ {total:.2f}")
+
+# -----------------------------------------------------------------
+
+# preco = int(input('insira o valor do produto: '))
+
+# preco -= preco * 0.05
+
+# print(f'o valor com desconto ficou por R${preco: .2f}')
+
+# ---------------------------------------------------------------------
+
+# numero = float(input('insira seu número: '))
+
+# if numero %2 == 0 and numero >= 0:
+#     print('par e positivo')
+# elif numero %2 != 0 and numero >= 0:
+#     print('impar e positivo')
+# elif numero %2 == 0 and numero < 0:
+#     print('par e negativo')
+# else:
+#     print('impar e negativo')
+
+# n = float(input('insira seu número: '))
+
+# # msg = 'negativo e ' if n < 0 else 'positivo e '
+# # msg += 'impar' if n % 2 else 'par'
+
+# # print(msg)
+
+# m1 = 'negativo e '
+# if n >= 0:
+#     m1 = 'positivo e '
+
+# m2 = 'impar'
+# if n%2 == 0:
+#     m2 = 'par'
+
+# print(f"{m1}{m2}")
+
+# ---------------------------------------------------
+
+# hora = int(input('hora: '))
+
+# if hora <= 5:
+#     print('boa madrugada')
+# elif hora <= 11:
+#     print('bom dia')
+# elif hora <= 17:
+#     print('boa tarde')
+# elif hora <= 23:
+#     print('boa noite') #subir isso, transformar <0 hora >23 em if
+# else:
+#     print('error')
+
+#------------------------------------------------------
+
